c37e534c428c1a458dbe9b02882c8ab5c74165d3
Carl Egbert <egbertcarl@gmail.com>
2023-12-05 22:56:42 -0800

    horribly slow and also just horrible part 2 solution

--- solutions/p5.rkt
+++ solutions/p5.rkt
@@ -11,8 +11,7 @@
 (define (get-map-blocks fname)
   (~> fname
       file->string
-      (string-split "\n\n")
-      cdr))
+      (string-split "\n\n")))
 
 (define (check-block-line source line)
   (let* ([nums (~>> line string-split (map string->number))]
@@ -38,14 +37,55 @@
          map-blocks))
 
 (define (solve-p1 fname)
-  (define map-blocks (get-map-blocks fname))
+  (define map-blocks (cdr (get-map-blocks fname)))
 
   (~>> fname
        get-seeds
        (map (lambda (seed) (check-seed map-blocks seed)))
        (apply min)))
 
-(define (solve-p2 fname) 0)
+(define (check-block-line-backwards dest line)
+  (let* ([nums (~>> line string-split (map string->number))]
+         [dest-start (car nums)]
+         [source-start (cadr nums)]
+         [range-length (caddr nums)]
+         [dest-end (+ dest-start range-length)])
+    (if (and (>= dest dest-start) (< dest dest-end))
+      (+ source-start (- dest dest-start))
+      #f)))
+
+(define (check-seed-block dest seed-block)
+  (~>> seed-block
+       (regexp-match* #rx"[0-9]+ [0-9]+")
+       (map (lambda~>> string-split
+                       (map string->number)))
+       (ormap (lambda (r) (and (<= (car r) dest)
+                               (> (+ (car r) (cadr r)) dest)
+                               dest)))))
+
+(define (check-block-backwards dest map-block)
+  (if (regexp-match #rx"seeds: " map-block)
+    (check-seed-block dest map-block)
+    (~>> map-block
+         (string-split _ "\n")
+         cdr
+         (ormap (lambda (block-line) (check-block-line-backwards dest block-line)))
+         (or _ dest))))
+
+(define (check-backwards map-blocks location)
+  (and
+    (foldr (lambda (map-block dest)
+             (check-block-backwards dest map-block))
+           location
+           map-blocks)
+    location))
+
+(define (stream-from n)
+  (stream-cons n (stream-from (add1 n))))
+
+(define (solve-p2 fname) 
+  (define map-blocks (get-map-blocks fname))
+  (stream-ormap (lambda (x) (check-backwards map-blocks x)) (stream-from 1)))
 
 (module+ test
   (require
@@ -63,7 +103,7 @@
                 (test-equal?
                   "part 2 with sample input"
                   (solve-p2 input-file)
-                  0)))
+                  46)))
 
   (run-tests suite))
 
