becb51ad0d4ae192d384f625c6742c30a686c664
Carl Egbert <egbertcarl@gmail.com>
2023-12-08 00:33:12 -0800

    day 7 part 2

--- solutions/p7.rkt
+++ solutions/p7.rkt
@@ -13,10 +13,16 @@
          [bid (string->number (cadr parts))])
     (hand (car parts) bid)))
 
-(define (get-matches hand)
+(define (filter-jacks-if-wild wild cards)
+  (if wild
+    (filter (lambda (card) (not (equal? card #\J))) cards)
+    cards))
+
+(define (get-matches hand wild)
   (~>> hand
        hand-cards
        string->list
+       (filter-jacks-if-wild wild)
        (sort _ char<?)
        (map string)
        (string-join _ "")
@@ -24,19 +30,42 @@
        (map (lambda~> string->list length))
        (sort _ <)))
 
-(define (score hand)
-  (match (get-matches hand)
-    ['(5) 10]
-    ['(4) 9]
-    ['(3 2) 8]
-    ['(2 3) 8]
-    ['(3) 7]
-    ['(2 2) 6]
-    ['(2) 5]
-    ['() 0]))
+(define (wildcards hand)
+  (~>> hand
+       hand-cards
+       (regexp-match* #rx"J")
+       length))
 
-(define (card->score card)
+(define (upgrade-hand-strength hand-strength wildcards)
+  (cond [(= wildcards 5) '(5)]
+        [(= wildcards 0) hand-strength]
+        [else (upgrade-hand-strength
+                (match hand-strength
+                  ['(4) '(5)]
+                  ['(3) '(4)]
+                  ['(2 2) '(3 2)]
+                  ['(2) '(3)]
+                  ['() '(2)])
+                (sub1 wildcards))]))
+
+(define (score hand wild)
+  (let* ([matches (get-matches hand wild)]
+        [wildcard-count (if wild (wildcards hand) 0)]
+        [matches (upgrade-hand-strength matches wildcard-count)])
+    (match matches
+      ['(5) 10]
+      ['(4) 9]
+      ['(3 2) 8]
+      ['(2 3) 8]
+      ['(3) 7]
+      ['(2 2) 6]
+      ['(2) 5]
+      ['() 0])))
+
+(define (card->score card wild)
   (match card
+    [#\J (if wild 0 11)]
+
     [#\1 1]
     [#\2 2]
     [#\3 3]
@@ -47,25 +76,28 @@
     [#\8 8]
     [#\9 9]
     [#\T 10]
-    [#\J 11]
     [#\Q 12]
     [#\K 13]
     [#\A 14]))
 
-(define (tiebreak-lt hand-a hand-b)
-  (let ([a-score (card->score (car hand-a))]
-        [b-score (card->score (car hand-b))])
+(define (tiebreak-lt hand-a hand-b wild)
+  (let ([a-score (card->score (car hand-a) wild)]
+        [b-score (card->score (car hand-b) wild)])
     (if (= a-score b-score)
-      (tiebreak-lt (cdr hand-a) (cdr hand-b))
+      (tiebreak-lt (cdr hand-a) (cdr hand-b) wild)
       (< a-score b-score))))
 
-(define (hand-lt a b)
-  (let ([a-score (score a)]
-        [b-score (score b)])
+(define (hand-lt a b [wild #f])
+  (let ([a-score (score a wild)]
+        [b-score (score b wild)])
     (cond [(< a-score b-score) #t]
           [(> a-score b-score) #f]
           [else (tiebreak-lt (string->list (hand-cards a))
-                             (string->list (hand-cards b)))])))
+                             (string->list (hand-cards b))
+                             wild)])))
+
+(define (hand-lt-wild a b)
+  (hand-lt a b #t))
 
 (define (calculate-winnings hands)
   (define (iter hands rank)
@@ -83,7 +115,13 @@
        (calculate-winnings)
        (apply +)))
 
-(define (solve-p2 fname) 0)
+(define (solve-p2 fname)
+  (~>> fname
+       file->lines
+       (map parse-hand)
+       (sort _ hand-lt-wild)
+       (calculate-winnings)
+       (apply +)))
 
 (module+ test
   (require
@@ -100,7 +138,7 @@
                 (test-equal?
                   "part 2 with sample input"
                   (solve-p2 input-file)
-                  0)))
+                  5905)))
 
   (run-tests suite))
 
